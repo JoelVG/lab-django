74ab97ca4c602f9ac4fba0e7080657a7289fb0a3
JoelVG <cgjavg@gmail.com>
2021-03-25 13:53:17 -0400

    add gitignore and configure decouple for keys

--- firstapp/firstapp/settings.py
+++ firstapp/firstapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y-cs62i#ga8kz^do#r=#smuq4k68zzn=6&!xr19)8ahee^+e%3'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
